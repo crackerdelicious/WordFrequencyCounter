072ac5fb961ad270a1e2afbfb322c5ad811d3567
Chomp <Chomp@DESKTOP-VITLU9K>
2023-08-18 18:00:12 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace WordFrequencyCounter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "The quick brown fox jump over the lazy dog.";
+
+            string[] splitedWords = sentence.Split(' ', 9);
+
+            foreach (string word in splitedWords)
+            {
+                Console.WriteLine(word);
+            }
+
+            // CountWordFrequency(sentence);
+        }
+
+        static void CountWordFrequency(string sentence)
+        {
+            char[] punctuation = { '.', '-', '!', ':', ';', '&', '/', '\\', '|', '`', '\'', '?', ' ' };
+            string[] words = sentence.Split(punctuation);
+
+            foreach (string word in words)
+            {
+                Console.WriteLine(word.ToLower());
+            }
+        }
+    }
+}
